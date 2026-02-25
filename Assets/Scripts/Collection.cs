26887648e3216a0597567d5b4fab5151ae6767f1
Danug4 <danug4@gmail.com>
2026-02-25 17:28:59 +1100

    Added Asteroid Prefabs

---
+++ Assets/Scripts/Collection.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Collection : MonoBehaviour
+{
+
+    public GameObject particle;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+            
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Instantiate(particle, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+
+}
