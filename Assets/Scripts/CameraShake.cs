47cb2ce8dcb897729c09f4f55bdd5a422bce2604
Danug4 <danug4@gmail.com>
2026-03-25 18:57:42 +1100

    Started Camera Shake Effect

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -1,7 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class CameraShake : MonoBehaviour
 {
-   //Use a coroutine
-   //Apply as VFX to any relevant event e.g. shoot, destroy asteroid, die, ...
+    //Use a coroutine
+    //Apply as VFX to any relevant event e.g. shoot, destroy asteroid, die, ...
+    //Use Lerp?
+    //--------------------------
+
+    //Input: Duration, Intensity
+    //Store original stats of camera
+
+    //Do until exceed Duration:
+    //- Pick a random direction
+    //- Lerp in that direction/ Lerp scale based on the intensity
+    //- Pick a new direction / scale camera
+
+    //After exceed Duration:
+    //- Reset camera
+
+    Vector3 originalPos;
+    Vector3 originalScale;
+
+    private void Awake()
+    {
+        originalPos = transform.position;
+        originalScale = transform.lossyScale;
+    }
+    void ShakeCam(float duration, float intensity)
+    {
+        StartCoroutine(CameraShakeRoutine(duration, intensity));
+    }
+
+    IEnumerator CameraShakeRoutine(float duration, float intensity)
+    {
+        yield return null;
+    }
+
 }
